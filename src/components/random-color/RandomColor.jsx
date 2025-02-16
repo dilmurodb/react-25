2f15a87ca72aa1030b01cd0a739d7335365ceec4
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-16 20:22:15 +0500

    feat: implement Hex color generator function

--- src/components/random-color/RandomColor.jsx
+++ src/components/random-color/RandomColor.jsx
@@ -6,8 +6,17 @@ export default function RandomColor() {
 
     const [color, setColor] = useState('')
 
-    const handleHexColGen = () => {
+    const handleRandom = (length) => {
+        return Math.floor(Math.random() * length)
+    }
 
+    const handleHexColGen = () => {
+        const hexRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+        let randomHex = '#'
+        for ( let i = 0; i < 6; i++) {
+            randomHex += hexRange[handleRandom(hexRange.length)]
+        }
+        setColor(randomHex)
     }
     const handleRgbColGen = () => {
 
@@ -16,7 +25,7 @@ export default function RandomColor() {
 
     }
 
-    return <div className="random-color">
+    return <div className="random-color" style={{backgroundColor: `${color}`}}>
              <button onClick={handleHexColGen}>HEX Color</button>
              <button onClick={handleRgbColGen}>RGB Color</button>
              <button onClick={handleRandomColGen}>Random Color</button>
