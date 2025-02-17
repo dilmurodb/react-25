176b682b7dd3ad468eb24ed001832a7a3161bf9f
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-17 20:32:00 +0500

    refactor: delete commented out code

--- src/components/load-more-data/LoadMoreData.jsx
+++ src/components/load-more-data/LoadMoreData.jsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react'
 
 export default function LoadMoreData({ url, limit, skip }) {
     
-    // const [data, setData] = useState()
     const [arrayData, setArrayData] = useState([])
     const [skipNum, setSkipNum] = useState(skip)
     const [isLoading, setIsLoading] = useState(true)
@@ -13,9 +12,7 @@ export default function LoadMoreData({ url, limit, skip }) {
     const handleSkipNum = () => {
         let curNum = skipNum
         let newSkipNum = Number(curNum) + 20
-        // console.log(typeof(newSkipNum))
         let strNewSkipNum = newSkipNum.toString() 
-        // console.log(typeof(strNewSkipNum))
         setSkipNum(strNewSkipNum)
     }
 
@@ -24,7 +21,6 @@ export default function LoadMoreData({ url, limit, skip }) {
             const response = await fetch(`${url}?limit=${limit}&skip=${skipNum}&select=title,price`)
             if (!response.ok) throw Error('Did not receive expected data')
             const data = await response.json()
-            // setData(data)
             let destData = data.products
             let newData = arrayData.concat(destData)
             setArrayData(newData)
@@ -41,9 +37,6 @@ export default function LoadMoreData({ url, limit, skip }) {
         fetchItems()
     }, [url, skipNum])
 
-    // console.log(data)
-    // console.log(typeof(skipNum))
-    // console.log(arrayData)
 
     return <div className='load-more-data'>
             <div className='data-content'>
@@ -66,17 +59,6 @@ export default function LoadMoreData({ url, limit, skip }) {
                     : 
                     null
                 }
-                {/* {
-                    data && data.products.length > 0 ?
-                    data.products.map(item =>
-                    (<div key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>{item.price}</p>
-                    </div>)
-                    )
-                    : 
-                    null
-                } */}
             </div>
             <button disabled={arrayData.length >= 99 ? true : false } onClick={handleSkipNum}>Load more items</button>
            </div>
