52ea18f5fe14ae87182644eeb8a6c4cbf0113fc8
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-17 20:30:34 +0500

    feat: fetch data by clicking button fully functional

--- src/components/load-more-data/LoadMoreData.jsx
+++ src/components/load-more-data/LoadMoreData.jsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react'
 
 export default function LoadMoreData({ url, limit, skip }) {
     
-    const [data, setData] = useState()
-    // const [arrayData, setArrayData] = useState([])
+    // const [data, setData] = useState()
+    const [arrayData, setArrayData] = useState([])
     const [skipNum, setSkipNum] = useState(skip)
     const [isLoading, setIsLoading] = useState(true)
     const [errorMsg, setErrorMsg] = useState(null)
@@ -13,9 +13,9 @@ export default function LoadMoreData({ url, limit, skip }) {
     const handleSkipNum = () => {
         let curNum = skipNum
         let newSkipNum = Number(curNum) + 20
-        console.log(newSkipNum)
+        // console.log(typeof(newSkipNum))
         let strNewSkipNum = newSkipNum.toString() 
-        console.log(strNewSkipNum)
+        // console.log(typeof(strNewSkipNum))
         setSkipNum(strNewSkipNum)
     }
 
@@ -24,7 +24,10 @@ export default function LoadMoreData({ url, limit, skip }) {
             const response = await fetch(`${url}?limit=${limit}&skip=${skipNum}&select=title,price`)
             if (!response.ok) throw Error('Did not receive expected data')
             const data = await response.json()
-            setData(data)
+            // setData(data)
+            let destData = data.products
+            let newData = arrayData.concat(destData)
+            setArrayData(newData)
             setErrorMsg(null)
         } catch (err) {
             setErrorMsg(err.message)
@@ -33,16 +36,14 @@ export default function LoadMoreData({ url, limit, skip }) {
         }
     }
 
-    // const handleLoadMoreItems = () => {
-    //     console.log('button clicked')
-    // }
 
     useEffect(() => {
         fetchItems()
     }, [url, skipNum])
 
-    console.log(data)
-    console.log(skipNum)
+    // console.log(data)
+    // console.log(typeof(skipNum))
+    // console.log(arrayData)
 
     return <div className='load-more-data'>
             <div className='data-content'>
@@ -55,6 +56,17 @@ export default function LoadMoreData({ url, limit, skip }) {
                 : 
                 null}
                 {
+                    arrayData && arrayData.length > 0 ?
+                    arrayData.map(item =>
+                    (<div key={item.id}>
+                        <h3>{item.title}</h3>
+                        <p>{item.price}</p>
+                    </div>)
+                    )
+                    : 
+                    null
+                }
+                {/* {
                     data && data.products.length > 0 ?
                     data.products.map(item =>
                     (<div key={item.id}>
@@ -64,8 +76,8 @@ export default function LoadMoreData({ url, limit, skip }) {
                     )
                     : 
                     null
-                }
+                } */}
             </div>
-            <button onClick={handleSkipNum}>Load more items</button>
+            <button disabled={arrayData.length >= 99 ? true : false } onClick={handleSkipNum}>Load more items</button>
            </div>
 }
