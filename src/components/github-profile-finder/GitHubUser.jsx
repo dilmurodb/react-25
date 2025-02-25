f681e6a1ab922576e24e8a216aa541c001df16a2
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-25 21:26:57 +0500

    feat: change date format

--- src/components/github-profile-finder/GitHubUser.jsx
+++ src/components/github-profile-finder/GitHubUser.jsx
@@ -5,12 +5,17 @@ export default function GitHubUser({ user }) {
     
     const { avatar_url, created_at, html_url, name, login, public_repos } = user
 
+    const createdDate = new Date(created_at)
+
     return <div className="github-user">
                 <div>
                     <img className="github-user-img" src={avatar_url} />
-                    <p>Creation date: {created_at}</p>
+                    <p>Account creation date: {`${createdDate.getDate()}
+                                                 ${createdDate.toLocaleString('de-de', {month: 'short'})} 
+                                                 ${createdDate.getFullYear()}`}
+                    </p>
                     <p>Account url: {html_url}</p>
-                    <p>Name: {name}</p>
+                    <p>Name: {name || login}</p>
                     <p>Amount of public repositories: {public_repos}</p>   
                 </div>
            </div>
