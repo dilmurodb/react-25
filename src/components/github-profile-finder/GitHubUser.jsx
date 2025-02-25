ad53b7c405573c4cd2ae22afe1670fa259ccee69
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-25 21:07:53 +0500

    feat: GitHubUser search component fully functional

--- src/components/github-profile-finder/GitHubUser.jsx
+++ src/components/github-profile-finder/GitHubUser.jsx
@@ -1,12 +1,17 @@
 
 
 
-export default function GitHubUser({data}) {
+export default function GitHubUser({ user }) {
+    
+    const { avatar_url, created_at, html_url, name, login, public_repos } = user
+
     return <div className="github-user">
-                <img className="github-user-img" src={data.avatar_url} />
-                <p>{data.created_at}</p>
-                <p>{data.html_url}</p>
-                <p>{data.name}</p>
-                <p>{data.public_repos}</p>
+                <div>
+                    <img className="github-user-img" src={avatar_url} />
+                    <p>Creation date: {created_at}</p>
+                    <p>Account url: {html_url}</p>
+                    <p>Name: {name}</p>
+                    <p>Amount of public repositories: {public_repos}</p>   
+                </div>
            </div>
 }
--- src/components/github-profile-finder/ProfileFinder.jsx
+++ src/components/github-profile-finder/ProfileFinder.jsx
@@ -5,9 +5,8 @@ import { useState, useEffect } from 'react'
 
 export default function ProfileFinder({ url }) {
 
-    const [data, setData] = useState()
+    const [userData, setUserData] = useState()
     const [userName, setUserName] = useState('dilmurodb')
-    const [userNameTest, setUserNameTest] = useState('dilmurodb')
     const [isLoading, setIsLoading] = useState(false)
     const [errorMsg, setErrorMsg] = useState(null)
 
@@ -17,28 +16,28 @@ export default function ProfileFinder({ url }) {
             const response = await fetch(`${url}${userName}`)
             if (!response.ok) throw Error('Did not receive expected data')
             const res = await response.json()
-            console.log(res)
-            setData(res)
+            if (res) {
+                setUserData(res)
+            }
             setErrorMsg(null)
         } catch (err) {
             setErrorMsg(err.message)
         } finally {
             setIsLoading(false)
+            setUserName('')
         }
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(userNameTest)
-        setUserName(userNameTest)
+        fetchGitHubData()
+        setUserName(e.target.value)
     }
 
     useEffect(() => {
         fetchGitHubData()
-    }, [userName])
+    }, [])
 
-    console.log(data)
-    console.log(userNameTest)
 
     return <div className="github-profile-finder">
                 GitHub Profile Finder Component
@@ -49,12 +48,17 @@ export default function ProfileFinder({ url }) {
                     id="search"
                     type="text"
                     placeholder="Enter user name"
-                    value={userNameTest}
-                    onChange={(e) => setUserNameTest(e.target.value)} 
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)} 
                     />  
                   <button type="submit">Search</button>  
                 </form>
-                <GitHubUser
-                        data={data} />
+                {
+                    isLoading ? <h3>Data is Loading...</h3> : null
+                }
+                {
+                    errorMsg ? <h3>{errorMsg}</h3> : null
+                }
+                { userData ? <GitHubUser user={userData} /> : null}
            </div>
 }
