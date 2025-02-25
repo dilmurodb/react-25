286815fe933037dad10d5e0da959660afb9cbd80
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-25 12:29:37 +0500

    init: create ModalPopup component

--- src/components/custom-modal-popup/CustomModalPopup.jsx
+++ src/components/custom-modal-popup/CustomModalPopup.jsx
@@ -1,8 +1,10 @@
 import './CustomModalPopup.css'
+import ModalPopup from './ModalPopup'
 
 
 export default function CustomModalPopup() {
     return <div className="custom-modal-popup">
                 <h1>Custom Modal Popup component!</h1>
+                <ModalPopup />
            </div>
 }
---
+++ src/components/custom-modal-popup/ModalPopup.jsx
@@ -0,0 +1,8 @@
+
+
+
+export default function ModalPopup() {
+    return <div>
+                Modal Popup Component
+           </div>
+}
