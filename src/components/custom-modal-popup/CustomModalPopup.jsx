2159d67a071520a74568c9d9a3c52b82136555f5
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-25 13:57:38 +0500

    feat: CustomModalPopup is fully functional

--- src/components/custom-modal-popup/CustomModalPopup.jsx
+++ src/components/custom-modal-popup/CustomModalPopup.jsx
@@ -1,10 +1,32 @@
 import './CustomModalPopup.css'
 import ModalPopup from './ModalPopup'
+import { useState } from 'react'
 
 
 export default function CustomModalPopup() {
+
+    const [modalPopup, setModalPopup] = useState(false)
+
+    const handleModalPopup = () => {
+        setModalPopup(!modalPopup)
+    }
+
     return <div className="custom-modal-popup">
-                <h1>Custom Modal Popup component!</h1>
-                <ModalPopup />
+                {/* {
+                    !modalPopup && <h1>Custom Modal Popup component!</h1>
+                } */}
+                <div className="h1-and-btn-container">
+                    <h1>Custom Modal Popup component!</h1>
+                    <button className="custom-modal-popup-btn" onClick={handleModalPopup}>Show Modal Popup</button>
+                </div>
+                {/* {
+                    !modalPopup && <button className="custom-modal-popup-btn" onClick={handleModalPopup}>Show Modal Popup</button>
+                } */}
+                <div className="modal-popup-section">
+                  {
+                    modalPopup && <ModalPopup handleModalPopup={handleModalPopup} />
+                  }  
+                </div>
+                
            </div>
 }
--- src/components/custom-modal-popup/ModalPopup.jsx
+++ src/components/custom-modal-popup/ModalPopup.jsx
@@ -1,8 +1,21 @@
 
 
 
-export default function ModalPopup() {
-    return <div>
-                Modal Popup Component
+export default function ModalPopup({ handleModalPopup }) {
+    return <div className="modal-popup-container">
+               <div className="modal-popup-header">
+                <div className="modal-popup-header-h1">
+                   <h1>Header</h1> 
+                </div>
+                <div className="modal-popup-header-span">
+                    <span onClick={handleModalPopup} className="modal-popup-x">&times;</span>
+                </div>
+               </div>
+               <div className="modal-popup-body">
+                <p>Body</p>
+               </div>
+               <div className="modal-popup-footer">
+                <p>Footer</p>
+               </div>
            </div>
 }
