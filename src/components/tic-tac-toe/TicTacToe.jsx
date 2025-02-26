d959a66ebe1c09caaedd708b9cd7e4d6066d8324
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-26 16:15:51 +0500

    feat: implement logic of ticTacToe

--- src/components/tic-tac-toe/TicTacToe.jsx
+++ src/components/tic-tac-toe/TicTacToe.jsx
@@ -1,9 +1,23 @@
 import './TicTacToe.css'
+import { useState } from 'react'
 
 
 export default function TicTacToe() {
 
-    const ticTacToeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const [toggle, setToggle] = useState(true)
+    const [boxIndex, setBoxIndex] = useState()
+
+    // const ticTacToeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const ticTacToeArr = Array(9).fill('')
+
+    const handleClick = (index) => {
+        console.log(`${index} Box is clicked!`)
+        console.log(ticTacToeArr)
+        ticTacToeArr[index] = 'X'
+        console.log(ticTacToeArr)
+        setBoxIndex(index)
+        setToggle(!toggle)
+    }
 
     return <div className='tic-tac-toe'>
                 <div>
@@ -11,7 +25,22 @@ export default function TicTacToe() {
                 </div>
                 <div className='tic-tac-toe-container'>
                     {
-                        ticTacToeArr.map((box, index) => <div key={index} className='tic-tac-toe-box'></div>)
+                        ticTacToeArr.map((box, index) => 
+                        <div key={index} 
+                             className='tic-tac-toe-box'
+                             onClick={() => handleClick(index)}>
+                                {box}
+                                {/* {
+                                    toggle && index === boxIndex
+                                            ?
+                                        <span>X</span> : null
+                                }
+                                {
+                                    !toggle && index === boxIndex
+                                            ?
+                                        <span>O</span> : null
+                                } */}
+                        </div>)
                     }
                 </div>
            </div>
