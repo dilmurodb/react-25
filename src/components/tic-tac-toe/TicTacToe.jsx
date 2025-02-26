9f676ee334780b41142e0e8c26bc7b757e1d6574
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-26 14:29:53 +0500

    refactor: simplify TicTacToe container creation

--- src/components/tic-tac-toe/TicTacToe.jsx
+++ src/components/tic-tac-toe/TicTacToe.jsx
@@ -2,20 +2,17 @@ import './TicTacToe.css'
 
 
 export default function TicTacToe() {
+
+    const ticTacToeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
     return <div className='tic-tac-toe'>
                 <div>
                   <h1>Tic Tac Toe Game Component</h1>  
                 </div>
                 <div className='tic-tac-toe-container'>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
-                    <div className='tic-tac-toe-box'></div>
+                    {
+                        ticTacToeArr.map((box, index) => <div key={index} className='tic-tac-toe-box'></div>)
+                    }
                 </div>
            </div>
 }
